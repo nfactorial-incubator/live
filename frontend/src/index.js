fb437e2cff33770bf338c9d0a752ca2bbe283270
Samat Kalshabekov <skalshabekov@beeline.kz>
2022-06-06 11:56:31 +0600

    build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import "./externalScript";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
