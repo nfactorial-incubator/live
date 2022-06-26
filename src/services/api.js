be0237dbf1e36328ca3c730376921b93144100ac
Aidar Nugmanoff <a.nugmanoff@gmail.com>
2022-06-26 17:04:42 +0600

    add .env

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,6 @@ import { setupInterceptors } from "./interceptors";
 
 export const api = setupInterceptors(
   axios.create({
-    baseURL: "http://localhost:8080",
-    // baseURL: "https://node-api-refresh-token.herokuapp.com",
+    baseURL: process.env.REACT_APP_BASE_URL,
   })
 );
