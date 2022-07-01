c7cd2dd6c8dd487c973dced4a5ef5fb8417367c4
Aidar Nugmanoff <a.nugmanoff@gmail.com>
2022-07-01 17:59:49 +0600

    change button styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -16,7 +16,7 @@ export const Home = () => {
         <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <Link
             to="/register"
-            class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-600 "
           >
             Register
           </Link>
